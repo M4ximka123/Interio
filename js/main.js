6ee2374016a5d32186412e773f99b2860cecbdfb
M4ximka123 <max.show.200122@gmail.com>
2022-08-19 23:07:47 +0300

    creat burger menu

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.burger__menu').on('click', function () {
+        $('.header__list').toggleClass('header__list--active');
+    });
+
+});
+
+$('.burger__menu').click(function () {
+    $('.burger__menu:hover div:nth-child(1)').toggleClass('burger__animation-first');
+    $('.burger__menu:hover div:nth-child(2)').toggleClass('burger__animation-second');
+    $('.burger__menu:hover div:nth-child(3)').toggleClass('burger__animation-third');
+});
