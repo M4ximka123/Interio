0111d4c34383aeaee975328e8a2935bd0d8fb738
M4ximka123 <max.show.200122@gmail.com>
2022-08-21 11:59:59 +0300

    add animation

--- js/main.js
+++ js/main.js
@@ -11,3 +11,6 @@ $('.burger__menu').click(function () {
     $('.burger__menu:hover div:nth-child(2)').toggleClass('burger__animation-second');
     $('.burger__menu:hover div:nth-child(3)').toggleClass('burger__animation-third');
 });
+
+
+new WOW().init();
